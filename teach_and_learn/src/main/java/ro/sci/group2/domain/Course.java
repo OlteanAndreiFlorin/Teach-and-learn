20d3ee44c8f3a5530e70f9723df11ef3b214dd6f
Oltean Andrei-Florin <oltean.andrei.florin@gmail.com>
2016-02-01 15:53:41 +0200

    Added course dao's and in memory dao's. and tests for the Course class and service.

--- teach_and_learn/src/main/java/ro/sci/group2/domain/Course.java
+++ teach_and_learn/src/main/java/ro/sci/group2/domain/Course.java
@@ -1,12 +1,15 @@
 package ro.sci.group2.domain;
 
-public class Course {
+public class Course extends AbstractModel {
 
 	private String name;
 	private int level;
 
-	public Course() {
-		
+	public Course(long id) {
+		setId(id);
+	}
+	public Course(){
+		this(0);
 	}
 
 	public String getName() {
@@ -31,7 +34,7 @@ public class Course {
 		if (name.isEmpty()) {
 			return false;
 		}
-		if(name.length()>20){
+		if(name.length()<2||name.length()>20){
 			return false;
 		}
 		for (char c : name.toCharArray()) {
