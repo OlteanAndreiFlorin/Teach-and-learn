28df6fd01a5f401cf019c9dcb863a01824e15477
razvan.radu587@gmail.com <razvan.radu587@gmail.com>
2016-02-08 22:38:29 +0200

    Added new feature - teacher competences (courses)
    Added via the TeacherController and 3 brand new HTML files

--- teach_and_learn/src/main/java/ro/sci/group2/web/TeacherController.java
+++ teach_and_learn/src/main/java/ro/sci/group2/web/TeacherController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ro.sci.group2.annotation.CurrentUser;
+import ro.sci.group2.domain.Course;
 import ro.sci.group2.domain.User;
+import ro.sci.group2.service.CourseService;
 import ro.sci.group2.service.UserService;
 
 @Controller
@@ -17,13 +19,8 @@ public class TeacherController {
 
 	@Autowired
 	UserService userService;
-	
-	/*@RequestMapping("")
-	public ModelAndView list(Long id, @CurrentUser org.springframework.security.core.userdetails.User u) {
-		ModelAndView view = new ModelAndView("teacher_index");
-		view.addObject("user", userService.findByUserName(u.getUsername()));
-		return view;
-	}*/
+	@Autowired
+	CourseService courseService;
 	
 	@RequestMapping("")
 	public ModelAndView list(Long id,@CurrentUser org.springframework.security.core.userdetails.User u) {
@@ -49,7 +46,7 @@ public class TeacherController {
 		return result;
 	}
 	
-	@RequestMapping("/meetings")
+	@RequestMapping("/teacher_meetings")
 	public ModelAndView onMeetingEdit(Long id, @CurrentUser org.springframework.security.core.userdetails.User u) {
 		ModelAndView result;
 
@@ -66,11 +63,77 @@ public class TeacherController {
 		return result;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(value="" , method = RequestMethod.POST)
 	public ModelAndView saveuser(User user, @CurrentUser org.springframework.security.core.userdetails.User u) {
 		userService.save(user);
 		ModelAndView view = new ModelAndView("teacher_index");
 		view.addObject(user);
 		return view;
 	}
+	
+	@RequestMapping("/teacher_courses")
+	public ModelAndView listCourses(Long id, @CurrentUser org.springframework.security.core.userdetails.User u) {
+		ModelAndView view = new ModelAndView("teacher_courses");
+		User user = new User();
+		if (id != null) {
+			user = userService.findById(id);
+		}
+		view.addObject("user", user);
+		view.addObject("teacherCourses", user.getCourses());
+		view.addObject("baseCourses", courseService.listAll());
+		return view;
+	}
+	
+	@RequestMapping(value="/teacher_courses_add")
+	public ModelAndView addCourse(Long id, Course course, @CurrentUser org.springframework.security.core.userdetails.User u) {
+		ModelAndView view = new ModelAndView("teacher_courses");
+		User user = new User();
+		if (id != null) {
+			user = userService.findById(id);
+			userService.addCourse(user.getId(), course);
+		}
+		view.addObject("user", user);
+		view.addObject("teacherCourses", user.getCourses());
+		view.addObject("baseCourses", courseService.listAll());
+		return view;
+	}
+	
+	@RequestMapping(value="/teacher_course_create")
+	public ModelAndView onCreate(Long id) {
+		
+		ModelAndView view = new ModelAndView("new_course");
+		User user = new User();
+		if (id != null) {
+			user = userService.findById(id);
+		}
+		view.addObject("user", user);
+		return view;
+	}
+	
+	@RequestMapping(value="/teacher_course_create", method=RequestMethod.POST)
+	public ModelAndView saveCourse(Course course, @CurrentUser org.springframework.security.core.userdetails.User u) {
+		courseService.save(course);
+		ModelAndView view = new ModelAndView("teacher_courses");
+		User user = new User();
+		user = userService.findByUserName(u.getUsername());
+		view.addObject("user", user);
+		view.addObject("teacherCourses", user.getCourses());
+		view.addObject("baseCourses", courseService.listAll());
+		return view;
+	}
+	
+	@RequestMapping(value="/teacher_course_remove")
+	public ModelAndView removeCourse(Long id, Course course, @CurrentUser org.springframework.security.core.userdetails.User u) {
+		ModelAndView view = new ModelAndView("teacher_courses");
+		User user = new User();
+		if (id != null) {
+			user = userService.findById(id);
+			userService.removeCourse(user.getId(), course);
+		}
+		view.addObject("user", user);
+		view.addObject("teacherCourses", user.getCourses());
+		view.addObject("baseCourses", courseService.listAll());
+		return view;
+	}
+	
 }
